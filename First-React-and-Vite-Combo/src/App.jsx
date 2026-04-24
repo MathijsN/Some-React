98746e1880e711dbb4762f63063fd113e7b689c7
MathijsN <72303334+MathijsN@users.noreply.github.com>
2026-04-24 10:52:22 +0200

    .

--- First-React-and-Vite-Combo/src/App.jsx
+++ First-React-and-Vite-Combo/src/App.jsx
@@ -1,14 +1,27 @@
 import { useState } from 'react'
+import { Fragment } from 'react';
 import './styles.css'
 
 function App() {
   const [count, setCount] = useState(0);
   return (
-    <>
+    <main>
+      <Link />
       <h1>{count}</h1>
       <button onClick={() => setCount(count + 1)}>
         Increment
       </button>
+      <footer>
+        <Link />
+      </footer>
+    </main>
+  )
+}
+
+function Link() {
+  return (
+    <>
+      <a href="#">Klik mij</a>
     </>
   )
 }
