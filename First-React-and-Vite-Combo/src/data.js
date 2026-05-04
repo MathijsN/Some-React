46d0a6e29a9af409926d27e29e5d88109486d461
MathijsN <72303334+MathijsN@users.noreply.github.com>
2026-05-04 11:54:21 +0200

    added more data

--- First-React-and-Vite-Combo/src/data.js
+++ First-React-and-Vite-Combo/src/data.js
@@ -14,4 +14,49 @@ export default [
         heading: 'Portfolio',
         link: 'https://github.com/MathijsN/Portfolio'
     },
+    {
+        id: 4,
+        heading: 'GitHub',
+        link: 'https://github.com/MathijsN'
+    },
+    {
+        id: 5,
+        heading: 'I love web',
+        link: 'https://github.com/MathijsN/i-love-web'
+    },
+    {
+        id: 6,
+        heading: 'Portfolio',
+        link: 'https://github.com/MathijsN/Portfolio'
+    },
+    {
+        id: 7,
+        heading: 'GitHub',
+        link: 'https://github.com/MathijsN'
+    },
+    {
+        id: 8,
+        heading: 'I love web',
+        link: 'https://github.com/MathijsN/i-love-web'
+    },
+    {
+        id: 9,
+        heading: 'Portfolio',
+        link: 'https://github.com/MathijsN/Portfolio'
+    },
+    {
+        id: 10,
+        heading: 'GitHub',
+        link: 'https://github.com/MathijsN'
+    },
+    {
+        id: 11,
+        heading: 'I love web',
+        link: 'https://github.com/MathijsN/i-love-web'
+    },
+    {
+        id: 12,
+        heading: 'Portfolio',
+        link: 'https://github.com/MathijsN/Portfolio'
+    },
 ]
