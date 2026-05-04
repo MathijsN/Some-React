c078a9b692a9d0052c8c745eb23afba918b094bf
MathijsN <72303334+MathijsN@users.noreply.github.com>
2026-05-04 11:21:08 +0200

    mapping

--- First-React-and-Vite-Combo/src/App.jsx
+++ First-React-and-Vite-Combo/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import { Fragment } from 'react';
 import './styles.css'
+import contentInfo from "./data.js"
 
 import Header from './components/header'
 
 function App() {
+  const newArr = contentInfo.map(content => {
+    return <Header content={content.heading} />
+  })
   return (
 
     <>
-      <Header content="nu is het deze header" />
+      <Header content={newArr} />
     </>
   )
 }
---
+++ First-React-and-Vite-Combo/src/data.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        id: 1,
+        heading: 'heading 1'
+    },
+    {
+        id: 2,
+        heading: 'heading 2'
+    },
+    {
+        id: 3,
+        heading: 'heading 3'
+    },
+    {
+        id: 4,
+        heading: 'heading 4'
+    },
+    {
+        id: 5,
+        heading: 'heading 5'
+    },
+]
